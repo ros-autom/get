fb40dc6d869ff662b3cc87907c30617dd14ef617
v.s.moisiadis@gmail.com <dkatikaridis@gmail.com>
2017-11-28 11:12:50 +0200

    product:Catch user's wrong or empty input

--- src/product.cpp
+++ src/product.cpp
@@ -3,8 +3,22 @@
 #include <iostream>
 #include <ros/publisher.h>
 #include "get/SendGoalArray.h"
+void warnEmpty();
+void warnWrong();
 int main(int argc, char **argv) {
+
   ros::init(argc, argv, "product_node");
+  if(argc < 2) {
+    warnEmpty();
+    return -1;
+  }
+  if (argv[1] != "CocaCola"
+    || argv[1] != "Fanta"
+    || argv[1] != "Qr"
+    || argv[1] != "Lays") {
+      warnWrong();
+      return -1;
+    }
 
   ros::NodeHandle n;
 
@@ -15,7 +29,7 @@ int main(int argc, char **argv) {
   ros::ServiceClient client = n.serviceClient<get::SendGoalArray>("SendGoalArray");
   get::SendGoalArray srv;
   srv.request.productName = argv[1];
-  if (client.call(srv)) {  
+  if (client.call(srv)) {
     std::cout<<"_________________"<<std::endl;
 
     for(int i=0;i<srv.response.size;i++)
@@ -30,4 +44,24 @@ int main(int argc, char **argv) {
   }
 
   return 0;
+}
+void warnWrong() {
+  ROS_WARN("Wrong product given.");
+  ROS_WARN("<product> can be:");
+  ROS_WARN(" - CocaCola");
+  ROS_WARN(" - Fanta");
+  ROS_WARN(" - Lays");
+  ROS_WARN(" - Qr");
+  ROS_WARN(" ");
+}
+void warnEmpty() {
+  ROS_WARN("No product given.");
+  ROS_WARN("rosrun get product <product>");
+  ROS_WARN(" ");
+  ROS_WARN("Where <product> can be:");
+  ROS_WARN(" - CocaCola");
+  ROS_WARN(" - Fanta");
+  ROS_WARN(" - Lays");
+  ROS_WARN(" - Qr");
+  ROS_WARN(" ");
 }
